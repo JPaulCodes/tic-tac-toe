80aad4433d6909178f621d02d759d4f656796ea7
JonPaulCode <jwatson88@live.co.uk>
2022-04-13 18:38:57 +0100

    Add gameboard functionality

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -3,3 +3,18 @@ const createPlayer = (name, marker) => {
   const getMarker = () => marker;
   return { getName, getMarker };
 };
+
+const gameboard = (() => {
+  const gameboardArray = new Array(9).fill(undefined);
+
+  const setField = (index, marker) => {
+    gameboardArray[index] = marker;
+  };
+
+  const checkField = (index) => gameboardArray[index];
+
+  return {
+    setField,
+    checkField,
+  };
+})();
